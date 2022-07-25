40e7304fce7d6bd18cc08fe251308ef641c70a2e
delarosa0305 <delarosa2381i@gmail.com>
2022-07-25 19:54:51 -0400

    single post handlebars was created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -42,21 +42,21 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
     })
-      .then(dbUserData => {
-        req.session.save(() => {
-          req.session.user_id = dbUserData.id;
-          req.session.username = dbUserData.username;
-          req.session.loggedIn = true;
-  
-          res.json(dbUserData);
-        });
-      })
-      .catch(err => res.status(500).json(err));
-  });
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
+        .catch(err => res.status(500).json(err));
+});
 
 router.post('/login', (req, res) => {
     User.findOne({
@@ -105,12 +105,15 @@ router.put('/:id', (req, res) => {
     })
         .then(dbUserData => {
             if (!dbUserData) {
-                res.status(400).json({ message: 'No user with that id' });
+                res.status(404).json({ message: 'No user found with this id' });
                 return;
             }
-            res.json(dbUserData)
+            res.json(dbUserData);
         })
-        .catch(err => res.status(500).json(err));
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.delete('/:id', (req, res) => {
